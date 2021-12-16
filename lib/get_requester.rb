66def78bcc99c04287333b4cc67327dace278205
aleksq7 <morrisalex2001@gmail.com>
2021-12-16 09:30:47 -0700

    commit tree cab109ac8fb94bb575609eb7975dd174c25879ac

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
 # Write your code here
+
+require 'net/http'
+require 'open-uri'
+
+class GetRequester
+
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(self.url)
+        Net::HTTP.get_response(uri).body
+    end
+
+    def parse_json
+        body = self.get_response_body
+        JSON.parse(body)
+    end
+end
